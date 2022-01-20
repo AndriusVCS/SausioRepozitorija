cb875afac4c43675c9ed6fbf1ad7de39c68aa244
Andrius <andriusjvcs@gmail.com>
2022-01-20 19:48:30 +0200

    Snipas

---
+++ PirmasProjektas/Klasiu-Uzdaviniai/Snipas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasiu_Uzdaviniai
+{
+    class Snipas
+    {
+        public string Vardas { get; private set; }
+        public int InfiltracijuKiekis { get; private set; }
+
+        public Snipas(string vardas)
+        {
+            Vardas = vardas;
+            InfiltracijuKiekis = 0;
+        }
+
+        public void Infiltruotis()
+        {
+            InfiltracijuKiekis++;
+        }
+    }
+}
