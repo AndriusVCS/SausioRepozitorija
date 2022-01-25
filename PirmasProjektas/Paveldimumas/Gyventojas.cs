ed756f9516d4c6168208109122a04c395e88c200
Andrius <andriusjvcs@gmail.com>
2022-01-25 12:06:41 +0200

    Paveldimumo uzdaviniai

---
+++ PirmasProjektas/Paveldimumas/Gyventojas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Paveldimumas
+{
+    public class Gyventojas : Pilietis
+    {
+        public string GyvenamojiVieta { get; }
+
+        public Gyventojas(string vardas, string pavarde, int asmensKodas, string pilietybe, string gyvenamojiVieta) : base(vardas, pavarde, asmensKodas, pilietybe)
+        {
+            GyvenamojiVieta = gyvenamojiVieta;
+        }
+
+        public override void Info()
+        {
+            base.Info();
+            Console.WriteLine($"Mano gyvenamoji vieta: {GyvenamojiVieta}");
+        }
+
+    }
+}
---
+++ PirmasProjektas/Paveldimumas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Paveldimumas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZmogausUzdavinys();
+            Console.ReadLine();
+        }
+
+        private static void ZmogausUzdavinys()
+        {
+            Zmogus zmogus = new Zmogus("Petras", "Petrauskas");
+            Pilietis pilietis = new Pilietis("Jonas", "Jonaitis", 123456789, "Lietuvis");
+            Gyventojas gyventojas = new Gyventojas("Lukas", "Lukaitis", 987654321, "Anglas", "Londonas");
+
+            zmogus.Info();
+            Console.WriteLine("************");
+            pilietis.Info();
+            Console.WriteLine("************");
+            gyventojas.Info();
+        }
+
+        private static void KaladesUzdavinys()
+        {
+            KortuKalade kalade = new KortuKalade();
+
+            foreach (Korta korta in kalade.KortuSarasas)
+            {
+                Console.WriteLine($"{korta.Pavadinimas} {korta.Verte} {korta.Zenklas}");
+            }
+
+            Console.WriteLine($"Viso kortu: {kalade.KortuSarasas.Count}");
+        }
+    }
+}
